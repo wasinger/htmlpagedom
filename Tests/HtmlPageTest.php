436347a44c3acfcae20b557cb66a6e4fa8ebaeea
Christoph Singer <dev@ccss.de>
2022-04-12 12:56:39 +0200

    Test result should keep newlines

--- Tests/HtmlPageTest.php
+++ Tests/HtmlPageTest.php
@@ -90,11 +90,17 @@ END;
 
         $expected =<<<END
 <!DOCTYPE html>
-<html><head><title></title><script>
+<html>
+<head>
+<title></title>
+<script>
 // this will be awesome
 alert('Hello world');
-</script></head><body>
-<h1>Script Test</h1></body></html>
+</script>
+</head>
+<body>
+<h1>Script Test</h1></body>
+</html>
 
 END;
         $this->assertEquals($expected, $newhtml);
